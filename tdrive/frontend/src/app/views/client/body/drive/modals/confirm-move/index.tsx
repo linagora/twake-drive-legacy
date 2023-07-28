276a14b71518311e51ce0ffaad4a7d3265e75559
Esteban DECTOT <estebandectot@gmail.Com>
2023-07-28 14:14:41 +0200

    code refactor

--- tdrive/frontend/src/app/views/client/body/drive/modals/confirm-move/index.tsx
+++ tdrive/frontend/src/app/views/client/body/drive/modals/confirm-move/index.tsx
@@ -7,85 +7,63 @@ import { atom, useRecoilState } from 'recoil';
 import Languages from '@features/global/services/languages-service';
 
 export type ConfirmModalType = {
-  open: boolean;
-  //TODO[ASH] what does it mean "parent_id" in terms of ModalWindow?
-  parent_id: string;
-  mode: 'move' | 'select-file' | 'select-files';
-  title: string;
-  onSelected: (ids: string[]) => Promise<void>;
+    open: boolean;
+    parent_id: string;
+    mode: 'move' | 'select-file' | 'select-files';
+    title: string;
+    onSelected: (ids: string[]) => Promise<void>;
 };
 
 export const ConfirmModalAtom = atom<ConfirmModalType>({
-  key: 'ConfirmModalAtom',
-  default: {
-    open: false,
-    parent_id: '',
-    mode: 'move',
-    title: '',
-    onSelected: async () => {},
-  },
+    key: 'ConfirmModalAtom',
+    default: {
+        open: false,
+        parent_id: '',
+        mode: 'move',
+        title: '',
+        onSelected: async () => {},
+    },
 });
 
 export const ConfirmModal = () => {
-  const [state, setState] = useRecoilState(ConfirmModalAtom);
+    const [state, setState] = useRecoilState(ConfirmModalAtom);
 
-  return (
-    <Modal open={state.open} onClose={() => setState({ ...state, open: false })}>
-      <ConfirmModalContent key={state.parent_id} showfiles={false}/>
-    </Modal>
-  );
+    return (
+        <Modal open={state.open} onClose={() => setState({ ...state, open: false })}>
+            <ConfirmModalContent key={state.parent_id}/>
+        </Modal>
+    );
 };
 
+const ConfirmModalContent = () => {
+    const [state, setState] = useRecoilState(ConfirmModalAtom);
+    const [selected, setSelected] = useState<DriveItem[]>([]);
+    const [loading, setLoading] = useState(false);
+    const [parentId] = useState(state.parent_id);
 
-//TODO[ASH] and you don't use this params: "key" and "showfiles"
-const ConfirmModalContent = (key:any,showfiles:boolean) => {
-  const [state, setState] = useRecoilState(ConfirmModalAtom);
-  const [selected, setSelected] = useState<DriveItem[]>([]);
-  const [loading, setLoading] = useState(false);
-  const [parentId, setParentId] = useState(state.parent_id);
+    const {item: parent} = useDriveItem(parentId);
 
-  const { children, path, item: parent, refresh } = useDriveItem(parentId);
+    useEffect(() => {
+        if (state.mode === 'select-file' && parent) setSelected([]);
+        if (state.mode === 'move' && parent) setSelected([parent]);
+    }, [parentId, parent?.id]);
 
-  useEffect(() => {
-    if (state.mode === 'select-file' && parent) setSelected([]);
-    if (state.mode === 'move' && parent) setSelected([parent]);
-    refresh(parentId);
-  }, [parentId, parent?.id]);
-
-  //TODO[ASH] why do you need this?
-  const folders = (children?.filter(i => i.is_directory) || []).sort((a, b) =>
-    a.name.localeCompare(b.name),
-  );
-  const files = (children?.filter(i => !i.is_directory) || []).sort((a, b) =>
-    a.name.localeCompare(b.name),
-  );
-
-  return (
-    <ModalContent title={state.title}>
-      <Button
-        disabled={selected.length === 0}
-        loading={loading}
-        theme="primary"
-        className="float-right"
-        onClick={async () => {
-          setLoading(true);
-          await state.onSelected(selected.map(i => i.id));
-          setState({ ...state, open: false });
-          setLoading(false);
-        }}
-      >
-        # TODO[ASH] can we move this if the function near "state.mode === 'select-file' && parent"
-        # it will be more readable
-        {selected.length === 0 ? (
-          <>{Languages.t('components.SelectorModalContent_no_items')}</>
-        ) : state.mode === 'move' ? (
-          <>{Languages.t('components.SelectorModalContent_move_to')} '{selected[0]?.name}'</>
-        ) : selected.length > 1 ? (
-          <> {selected.length} {Languages.t('components.SelectorModalContent_select')} {Languages.t('components.SelectorModalContent_files')}</>
-        ) : (
-          <>{Languages.t('components.SelectorModalContent_select')} '{selected[0]?.name}'</>
-        )}
-      </Button>
-    </ModalContent>
-  );
+    return (
+        <ModalContent title={state.title}>
+            <Button
+                disabled={selected.length === 0}
+                loading={loading}
+                theme="primary"
+                className="float-right"
+                onClick={async () => {
+                    setLoading(true);
+                    await state.onSelected(selected.map(i => i.id));
+                    setState({ ...state, open: false });
+                    setLoading(false);
+                }}
+            >
+                <>{Languages.t('components.SelectorModalContent_move_to')} '{selected[0]?.name}'</>
+            </Button>
+        </ModalContent>
+    );
 };
