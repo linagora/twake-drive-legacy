5c2b5349fc0157f31c8d8914010e93c59239a89d
Anton SHEPILOV <ashepilov@linagora.com>
2023-09-11 15:20:41 +0200

    🌟Synchronization of the user with LDAP

--- tdrive/backend/utils/ldap-sync/src/index.ts
+++ tdrive/backend/utils/ldap-sync/src/index.ts
@@ -8,14 +8,24 @@ interface UserAttributes {
   email: string;
 }
 
-dotenv.config();
+export interface IApiServiceApplicationTokenRequestParams {
+  id: string;
+  secret: string;
+}
 
-console.log("Run script with the following env: ");
-console.log(process.env);
+export interface IApiServiceApplicationTokenResponse {
+  resource: {
+    access_token: {
+      time: number;
+      expiration: number;
+      value: string;
+      type: string;
+    };
+  };
+}
 
-// LDAP server configuration
 const ldapConfig = {
-  url: process.env.LDAP_URL|| "localhost",
+  url: process.env.LDAP_URL || "localhost",
   bindDN: process.env.LDAP_BIND_DN || "",
   bindCredentials: process.env.LDAP_BIND_CREDENTIALS || "",
   searchBase: process.env.LDAP_SEARCH_BASE || "dc=example,dc=com",
@@ -24,9 +34,64 @@ const ldapConfig = {
   version: 3,
 };
 
+const tdriveConfig = {
+    url: process.env.TDRIVE_URL || "http://localhost:4000/)",
+    credentials: {
+      id: process.env.TDRIVE_CREDENTIALS_ID || "application-name",
+      secret: process.env.TDRIVE_CREDENTIALS_SECRET || "application-secret",
+  }
+};
+
+const refreshToken = async (): Promise<string> => {
+  try {
+    const response = await axios.post<IApiServiceApplicationTokenRequestParams, { data: IApiServiceApplicationTokenResponse }>(
+      `${tdriveConfig.url.replace(/\/$/, '')}/api/console/v1/login`,
+      {
+        id: tdriveConfig.credentials.id,
+        secret: tdriveConfig.credentials.secret,
+      },
+      {
+        headers: {
+          Authorization: `Basic ${Buffer.from(`${tdriveConfig.credentials.id}:${tdriveConfig.credentials.secret}`).toString('base64')}`,
+        },
+      },
+    );
+
+    const {
+      resource: {
+        access_token: { value },
+      },
+    } = response.data;
+
+    //axiosClient.interceptors.response.use(this.handleResponse, this.handleErrors);
+
+    return value;
+  } catch (error) {
+    console.error('failed to get application token', error);
+    console.info('Using token ', tdriveConfig.credentials.id, tdriveConfig.credentials.secret);
+    console.info(`POST ${tdriveConfig.url.replace(/\/$/, '')}/api/console/v1/login`);
+    console.info(`Basic ${Buffer.from(`${tdriveConfig.credentials.id}:${tdriveConfig.credentials.secret}`).toString('base64')}`);
+    throw new Error("Unable to get access to token, see precious errors for details.");
+  }
+};
+
+dotenv.config();
+
+console.log("Run script with the following env: ");
+console.log(process.env);
+
 // Create LDAP client
 const client = ldap.createClient({
   url: ldapConfig.url,
+});
+
+const accessToken = await refreshToken()
+
+const axiosClient = axios.create({
+  baseURL: tdriveConfig.url,
+  headers: {
+    Authorization: `Bearer ${accessToken}`,
+  },
 });
 
 // Bind to LDAP server
@@ -62,7 +127,7 @@ client.bind(ldapConfig.bindDN, ldapConfig.bindCredentials, (err) => {
         };
 
         // Make API call to tdrive backend with the userAttributes
-        apiRequests.push(axios.post(process.env.API_URL || "", userAttributes));
+        apiRequests.push(axiosClient.post(process.env.API_URL || "", userAttributes));
       });
 
       searchRes.on("error", (err) => {
