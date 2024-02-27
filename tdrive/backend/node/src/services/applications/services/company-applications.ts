443e08d03fba67e1d3c3457e1d783112ac53c9f8
Anton SHEPILOV <ashepilov@linagora.com>
2024-02-27 12:37:40 +0100

    Revert "🔒 Do not expose application api into (#372)"
    This reverts commit 62845798b9c1c9bc209db6e272a1aab1038564f5.

--- tdrive/backend/node/src/services/applications/services/company-applications.ts
+++ tdrive/backend/node/src/services/applications/services/company-applications.ts
@@ -23,8 +23,6 @@ export class CompanyApplicationServiceImpl implements TdriveServiceProvider, Ini
         pk.application_id,
         context,
       );
-      //do not expose application secret
-      application.api = null;
 
       if (!application?.id) {
         return null;
@@ -64,8 +62,6 @@ export class CompanyApplicationServiceImpl implements TdriveServiceProvider, Ini
         companyApplication.application_id,
         context,
       );
-      //do not expose application secret
-      application.api = null;
       if (application)
         applications.push({
           ...companyApplication,
