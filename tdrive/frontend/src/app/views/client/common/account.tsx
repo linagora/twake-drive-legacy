3858145a956ad7d938185881130f7a506502c322
MontaGhanmy <monta.ghanmy@gmail.com>
2024-10-14 13:26:01 +0100

    fix: right alignement to the app icon

--- tdrive/frontend/src/app/views/client/common/account.tsx
+++ tdrive/frontend/src/app/views/client/common/account.tsx
@@ -1,5 +1,4 @@
 import Avatar from '@atoms/avatar';
-import { Base, Info } from '@atoms/text';
 import Menu from '@components/menus/menu';
 import LoginService from '@features/auth/login-service';
 import { useCurrentUser } from '@features/users/hooks/use-current-user';
@@ -7,9 +6,6 @@ import currentUserService from '@features/users/services/current-user-service';
 import AccountParameter from '@views/client/popup/UserParameter/UserParameter';
 import Languages from '../../../features/global/services/languages-service';
 import ModalManagerDepreciated from '@deprecated/popupManager/popupManager';
-import FeatureTogglesService, {
-  FeatureNames,
-} from '@features/global/services/feature-toggles-service';
 
 export default ({ sidebar }: { sidebar?: boolean }): JSX.Element => {
   const { user } = useCurrentUser();
@@ -47,19 +43,6 @@ export default ({ sidebar }: { sidebar?: boolean }): JSX.Element => {
         avatar={user.thumbnail}
         title={currentUserService.getFullName(user)}
       />
-      <div
-        className={'sm:block ml-2 mr-2 flex flex-col overflow-hidden ' + (sidebar ? '' : 'hidden')}
-      >
-        <Base className="font-bold overflow-hidden text-ellipsis whitespace-nowrap w-full block -mb-1">
-          {currentUserService.getFullName(user)}
-        </Base>
-
-        { !FeatureTogglesService.isActiveFeatureName(FeatureNames.COMPANY_DISPLAY_EMAIL) && (
-          <Info className="font-semibold overflow-hidden text-ellipsis whitespace-nowrap w-full">
-            {user.email}
-          </Info>
-        )}
-      </div>
     </Menu>
   );
 };
