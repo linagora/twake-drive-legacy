63ba61690055100dedab252c469f5313a9260ea2
MontaGhanmy <monta.ghanmy@gmail.com>
2024-10-15 10:43:27 +0100

    ref: added username, first name, and last name fields to user parameters modal

--- tdrive/frontend/src/app/views/client/popup/UserParameter/UserParameter.jsx
+++ tdrive/frontend/src/app/views/client/popup/UserParameter/UserParameter.jsx
@@ -1,22 +1,14 @@
 /* eslint-disable react/prop-types */
 /* eslint-disable react/jsx-key */
 import { Component } from 'react';
-
-import { getFilesTree } from '@components/uploads/file-tree-utils';
 import Collections from '@deprecated/CollectionsV1/Collections/Collections.js';
 import popupManager from '@deprecated/popupManager/popupManager.js';
 import currentUserService from '@deprecated/user/CurrentUser';
 import LoginService from '@features/auth/login-service';
-import InitService from '@features/global/services/init-service';
 import Languages from '@features/global/services/languages-service';
 import userService from '@features/users/services/current-user-service';
-import { ExternalLinkIcon } from '@heroicons/react/outline';
-import ButtonWithTimeout from 'components/buttons/button-with-timeout.jsx';
-import Input from 'components/inputs/input.jsx';
 import MenuList from 'components/menus/menu-component.jsx';
 import Attribute from 'components/parameters/attribute.tsx';
-import { Button } from '../../../../atoms/button/button';
-import * as Text from '../../../../atoms/text';
 import './UserParameter.scss';
 
 export default class UserParameter extends Component {
@@ -62,23 +54,29 @@ export default class UserParameter extends Component {
       return (
         <form className="" autoComplete="off">
           <div className="title">{this.state.i18n.t('scenes.apps.account.title')}</div>
-              <div className="group_section">
-                <Attribute
-                  label={this.state.i18n.t('scenes.apps.account.languages.menu_title')}
-                  description={this.state.i18n.t('scenes.apps.account.languages.text')}
+          <div className="group_section">
+            <Attribute
+              label={`${this.state.first_name} ${this.state.last_name}`}
+              description={`${this.state.username}`}
+            />
+          </div>
+          <div className="group_section">
+            <Attribute
+              label={this.state.i18n.t('scenes.apps.account.languages.menu_title')}
+              description={this.state.i18n.t('scenes.apps.account.languages.text')}
+            >
+              <div className="parameters_form">
+                <select
+                  value={this.state.i18n.language}
+                  onChange={ev => currentUserService.updateLanguage(ev.target.value)}
                 >
-                  <div className="parameters_form">
-                    <select
-                      value={this.state.i18n.language}
-                      onChange={ev => currentUserService.updateLanguage(ev.target.value)}
-                    >
-                      <option value="en">English</option>
-                      <option value="fr">Français</option>
-                      <option value="ru">Русский</option>
-                    </select>
-                  </div>
-                </Attribute>
+                  <option value="en">English</option>
+                  <option value="fr">Français</option>
+                  <option value="ru">Русский</option>
+                </select>
               </div>
+            </Attribute>
+          </div>
         </form>
       );
     }
